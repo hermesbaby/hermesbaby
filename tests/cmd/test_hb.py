97f15b1456148ec82cb17b60d426b954ecb8d567
Alexander Mann-Wahrenberg <alexander.mannwahrenberg@gmail.com>
2024-04-22 05:12:57 +0000

    Omnium rerum principia parva sunt.

---
+++ tests/cmd/test_hb.py
@@ -0,0 +1,14 @@
+""" CLI tests """
+
+import pytest
+
+from hermesbaby.skeleton import fib, main
+
+__author__ = "Alexander Mann-Wahrenberg (basaejumpa)"
+__copyright__ = "Alexander Mann-Wahrenberg (basejumpa)"
+__license__ = "MIT"
+
+
+def test_hb():
+    """CLI Tests"""
+    pass
