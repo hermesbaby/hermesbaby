1983557ca6c147199a1e0f8147b1baf2693b2d48
basejumpa <alexander.mannwahrenberg@gmail.com>
2024-12-27 14:12:28 +0100

    Prepare for adding new tasks

--- src/hermesbaby/__main__.py
+++ src/hermesbaby/__main__.py
@@ -1,10 +1,13 @@
+import argparse
 import os
+import sys
 from paver import tasks as paver
 
 
 def main():
+    options = sys.argv[1:] or ["hello"]
     pavement_path = os.path.join(os.path.dirname(__file__), "pavement.py")
-    paver.main(["--file", pavement_path, "--quiet", "hello"])
+    paver.main(["--file", pavement_path, "--quiet"] + options)
 
 
 if __name__ == "__main__":
