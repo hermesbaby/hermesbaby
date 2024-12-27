40d7925f40b48a131cca233e524a81f65257668c
basejumpa <alexander.mannwahrenberg@gmail.com>
2024-12-27 11:43:40 +0100

    Make hermesbaby say hello

--- tests/test_hermesbaby.py
+++ tests/test_hermesbaby.py
@@ -2,16 +2,31 @@ import subprocess
 import sys
 
 
-def test_no_args():
-    # Use the installed package directly
-    result = subprocess.run(
-        [sys.executable, "-m", "hermesbaby"],
-        stdout=subprocess.PIPE,
-        stderr=subprocess.PIPE,
-        text=True,
-    )
+def test_entry_points():
+    """Test the entry points"""
 
-    # Assert the output and return code
-    assert result.returncode == 0, f"Unexpected return code: {result.returncode}"
-    assert result.stdout.strip() == "Hermesbaby", f"Unexpected output: {result.stdout}"
-    assert result.stderr.strip() == "", f"Unexpected error output: {result.stderr}"
+    expected_stdout = "Hello"
+    expected_stderr = ""
+
+    entry_points = {
+        "as-module": [sys.executable, "-m", "hermesbaby"],
+        "as-script-long": ["poetry", "run", "hermesbaby"],
+        "as script-short": ["poetry", "run", "hb"],
+    }
+
+    for _, call in entry_points.items():
+
+        result = subprocess.run(
+            call,
+            stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE,
+            text=True,
+        )
+
+        assert result.returncode == 0, f"Unexpected return code: {result.returncode}"
+        assert (
+            result.stdout.strip() == expected_stdout
+        ), f"Unexpected output: {result.stdout}"
+        assert (
+            result.stderr.strip() == expected_stderr
+        ), f"Unexpected error output: {result.stderr}"
