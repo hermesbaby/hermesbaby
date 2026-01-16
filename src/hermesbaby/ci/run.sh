df6cbb2414349dd98a92cc9ca7f37e36ce323123
basejumpa <alexander.mannwahrenberg@gmail.com>
2026-01-16 12:29:05 +0100

    (ci) add multi-doc support

--- src/hermesbaby/ci/run.sh
+++ src/hermesbaby/ci/run.sh
@@ -57,22 +57,58 @@ echo "Populating environment from CI options JSON file at: $HERMESBABY_CI_OPTION
 eval $(hb ci config-to-env "$HERMESBABY_CI_OPTIONS_JSON_PATH")
 
 
+# Make sure that there is a .hermesbaby file even the project doesn't have one
+# Also make sure that the .hermesbaby file contains most recent parameters
+hb configure --update
+
+
+### DISCOVER DOCUMENTS ########################################################
+
+HERMESBABY_FILES=()
+
+discover_documents() {
+    echo ">>> Discovering HermesBaby project files in workspace"
+
+    # Search for .hermesbaby files in a simple and robust manner. Do not use any environment variables
+    mapfile -t HERMESBABY_FILES < <( \
+        find . \
+            \( -path ./.git \
+                -o -path '*/_attachments' \
+                -o -path '*/_figures' \
+                -o -path '*/_listings' \
+            \) \
+            -prune \
+            -o -name .hermesbaby -print \
+            | awk '{ depth = gsub("/", "/", $0); print depth, $0 }' \
+            | sort -nr -k1,1 -k2,2 \
+            | awk '{ $1=""; sub(/^ /,""); print }'
+    )
+
+    # Print discovered files
+    echo "Discovered ${#HERMESBABY_FILES[@]} HermesBaby project(s):"
+    for rel in "${HERMESBABY_FILES[@]}"; do
+        echo " - ${rel#./}"
+    done
+}
+
+
 ### BUILD #####################################################################
 
 build() {
-    echo "Building HermesBaby project to HTML."
-
-    # Inject hermesbaby project configuration into environment
-
-    # Make sure that there is a .hermesbaby file even the project doesn't have one
     # Also make sure that the .hermesbaby file contains most recent parameters
     hb configure --update
+
+    # Inject hermesbaby project configuration into environment
 
     # Strip possible trailing \r from each line
     sed -i 's/\r$//' .hermesbaby
 
-    # Inject into environment
+    # Inject into environment and make it available across
     source .hermesbaby
+
+    mkdir -p "$CONFIG_BUILD__DIRS__BUILD"/html
+    touch "$CONFIG_BUILD__DIRS__BUILD"/html/index.html
+    ls "$CONFIG_BUILD__DIRS__BUILD"/html/
 
     # Build HTML
     hb html
@@ -82,6 +118,7 @@ build() {
     # - the .hermesbaby file
     # - the build_parameters.json file
     if [ "${CONFIG_PUBLISH__CREATE_AND_EMBED_PDF:-n}" == "y" ]; then
+        echo "### Building HermesBaby project to PDF in $PWD"
         hb pdf
         pdf_file=$(basename $(ls "$CONFIG_BUILD__DIRS__BUILD"/pdf/*.tex) .tex).pdf
         cp "$CONFIG_BUILD__DIRS__BUILD"/pdf/$pdf_file "$CONFIG_BUILD__DIRS__BUILD"/html
@@ -92,7 +129,9 @@ build() {
 ### PACKAGE ###################################################################
 
 package() {
-    echo "Packaging HTML output into tar.gz archive."}
+    # Inject into environment and make it available across
+    source .hermesbaby
+
     tar -czf \
         $CONFIG_BUILD__DIRS__BUILD/html.tar.gz \
         -C $CONFIG_BUILD__DIRS__BUILD/html \
@@ -103,7 +142,8 @@ package() {
 ### PUBLISH ###################################################################
 
 publish () {
-    echo "Publishing HTML archive to Hermes server."
+    # Inject into environment and make it available across
+    source .hermesbaby
 
     # Check if publishing should be skipped
     if [ "${CONFIG_PUBLISH_SKIP_PUBLISH:-n}" == "y" ]; then
@@ -122,9 +162,20 @@ publish () {
 
 ### EXECUTION FLOW ############################################################
 
-build
-package
-publish
+loop_over() {
+    local step=$1
+    for rel in "${HERMESBABY_FILES[@]}"; do
+        pushd "$(dirname "$rel")" > /dev/null
+        echo ">>> Running step '$step' for project: ${rel#./}"
+        $step
+        popd > /dev/null
+    done
+}
+
+discover_documents
+loop_over build
+loop_over package
+loop_over publish
 
 ### END OF WORKFLOW ###########################################################
 
